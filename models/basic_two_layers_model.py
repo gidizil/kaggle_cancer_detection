15feb529a68b17dbdbe6ff38fb67b98fe8ff6dcf
gidizil <gidizil@gmail.com>
2019-04-30 09:13:01 +0300

    changed FC layer architecture

--- models/basic_two_layers_model.py
+++ models/basic_two_layers_model.py
@@ -3,14 +3,14 @@ import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net).__init__()
+        super(Net, self).__init__()
         #TODO: Add config options
-        self.conv1 = nn.conv2d(3, 16, 5)
+        self.conv1 = nn.Conv2d(3, 16, 5)
         self.conv1_bn_2d = nn.BatchNorm2d(16)
         self.pool = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(16, 128, 5)
         self.conv2_bn_2d = nn.BatchNorm2d(128)
-        self.fc1 = nn.Linear(128*5*5, 120)
+        self.fc1 = nn.Linear(128*21*21, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -27,7 +27,7 @@ class Net(nn.Module):
         x = self.pool(x)
 
         # 2. Feed Forward part of network
-        x = x.view(-1, 128 * 5 * 5)
+        x = x.view(-1, 128 * 21 * 21)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
