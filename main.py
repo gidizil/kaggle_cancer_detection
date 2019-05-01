fef425035e01f30068b868f4460576d1f6ca5907
gidizil <gidizil@gmail.com>
2019-05-01 23:50:49 +0300

    adjustements to fit data_utils_v2

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import torch
 import torchvision
-from data_utils import CancerDataset
+from data_utils_v2 import CancerDataset
+from data_utils_v2 import PickleImageData
 import configparser
 from models.basic_two_layers_model import Net
 from models.train_classifier import Classifier
+import os
 
 # 1. Get pickled data path
 config = configparser.ConfigParser()
 config.read_file(open(r'config.txt'))
-PICKLE_TRAIN_PATH = config.get('PATHS', 'PICKLE_TRAIN_PATH')
+PICKLE_S_TRAIN_PATH_V2 = config.get('PATHS', 'PICKLE_S_TRAIN_PATH_V2')
 
-train_set = CancerDataset(PICKLE_TRAIN_PATH)
+# 2. get pickled data objects (unpickled)
+images_list_path = os.path.join(PICKLE_S_TRAIN_PATH_V2, 'images_names_list.pickle')
+images_list = PickleImageData.unpickle_file(images_list_path)
+labels_dict_path = os.path.join(PICKLE_S_TRAIN_PATH_V2, 'labels_dict.pickle')
+labels_dict = PickleImageData.unpickle_file(labels_dict_path)
+
+
+train_set = CancerDataset(PICKLE_S_TRAIN_PATH_V2, labels_dict, images_list)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64,
                                            shuffle=True, num_workers=2)
 
