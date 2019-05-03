40d2d31c51d930c67357d36358c65185df8ad5c3
gidizil <gidizil@gmail.com>
2019-05-03 12:01:27 +0300

    general methods to help with misc issues

---
+++ general_utils.py
@@ -0,0 +1,33 @@
+import  random
+import os
+import numpy as np
+import torch
+
+""" =================================== """
+""" Collection of miscellaneous methods """
+""" to help with general problem        """
+""" encountered along the way            """
+""" =================================== """
+
+
+class GeneralUtils:
+    """Collection of useful methods"""
+
+    @staticmethod
+    def seed_torch(seed=1029):
+        """Setting a fixed seed for torch and it's dependencies"""
+        random.seed(seed)
+        os.environ['PYTHONHASHSEED'] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+    @staticmethod
+    def _init_fn(worker_id):
+        """ Setting a fixed seed for each worker in the DataLoader"""
+        np.random.seed(12 + worker_id)
+
+
