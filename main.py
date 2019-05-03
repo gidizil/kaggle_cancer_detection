35e90ffe1af65474888ba9c035200b7f8f92a1ec
gidizil <gidizil@gmail.com>
2019-05-03 12:02:49 +0300

    added support for fixed seed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import configparser
 from models.basic_two_layers_model import Net
 from models.train_classifier import Classifier
 import os
+from general_utils import GeneralUtils
 
 # 1. Get pickled data path
 config = configparser.ConfigParser()
@@ -18,10 +19,14 @@ images_list = PickleImageData.unpickle_file(images_list_path)
 labels_dict_path = os.path.join(PICKLE_S_TRAIN_PATH_V2, 'labels_dict.pickle')
 labels_dict = PickleImageData.unpickle_file(labels_dict_path)
 
+# 3. setting seeds:
+GeneralUtils.seed_torch()
+
 
 train_set = CancerDataset(PICKLE_S_TRAIN_PATH_V2, labels_dict, images_list)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64,
-                                           shuffle=True, num_workers=2)
+                                           shuffle=True, num_workers=2,
+                                           worker_init_fn=GeneralUtils._init_fn)
 
 net = Net()
 net_classifier = Classifier(net)
