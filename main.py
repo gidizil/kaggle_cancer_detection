fe05d1d5442d3e5184300b463873989171914ec3
gidizil <gidizil@gmail.com>
2019-05-16 11:37:39 +0300

    changed to work with cpu/gpu configuration

--- main.py
+++ main.py
@@ -11,20 +11,23 @@ from general_utils import SetSeeds, GPUConfig
 # 1. Get pickled data path
 config = configparser.ConfigParser()
 config.read_file(open(r'config.txt'))
-PICKLE_S_TRAIN_PATH_V2 = config.get('PATHS', 'PICKLE_S_TRAIN_PATH_V2')
+class_config = GPUConfig()
+path_dict = class_config.get_paths_dict()
+# PICKLE_S_TRAIN_PATH_V2 = config.get('PATHS', 'PICKLE_S_TRAIN_PATH_V2')
+# PICKLE_S_VAL_PATH_V2 = config.get('PATHS','PICKLE_S_VAL_PATH_V2')
 
-# 2. get pickled data objects (unpickled)
-images_list_path = os.path.join(PICKLE_S_TRAIN_PATH_V2, 'images_names_list.pickle')
+# 2A. get pickled data objects (unpickled) for training
+images_list_path = os.path.join(path_dict['train'], 'images_names_list.pickle')
 images_list = PickleImageData.unpickle_file(images_list_path)
-labels_dict_path = os.path.join(PICKLE_S_TRAIN_PATH_V2, 'labels_dict.pickle')
+labels_dict_path = os.path.join(path_dict['train'], 'labels_dict.pickle')
 labels_dict = PickleImageData.unpickle_file(labels_dict_path)
 
 # 3. setting seeds:
 SetSeeds.seed_torch()
 
-# 4. Build Dataset and DataLoader
-train_set = CancerDataset(PICKLE_S_TRAIN_PATH_V2, labels_dict, images_list)
-NUM_WORKERS = GPUConfig.set_num_workers(torch.cuda.is_available())
+# 4. Build Dataset and DataLoader for training
+train_set = CancerDataset(path_dict['train'], labels_dict, images_list)
+NUM_WORKERS = GPUConfig.set_num_workers()
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64,
                                            shuffle=True, num_workers=NUM_WORKERS,
@@ -35,3 +38,11 @@ net = Net()
 net_classifier = Classifier(net)
 
 net_classifier.fit(train_loader)
+
+# 6. Predict on new samples
+images_list_path = os.path.join(path_dict['val'], 'images_names_list.pickle')
+images_list = PickleImageData.unpickle_file(images_list_path)
+labels_dict_path = os.path.join(path_dict['val'], 'labels_dict.pickle')
+labels_dict = PickleImageData.unpickle_file(labels_dict_path)
+
+# TODO: finish the rest to be able to make predicitions on new data.
