e1a9c8dc7fb47b092902c9ec8bee06ad6127837d
gidizil <gidizil@gmail.com>
2019-05-16 11:36:56 +0300

    added method to generate gpu/cpu path dict

--- general_utils.py
+++ general_utils.py
@@ -36,17 +36,42 @@ class SetSeeds:
 class GPUConfig:
     """Different configurations for the case of GPU/CPU"""
 
-    @staticmethod
-    def set_num_workers(has_gpu):
+    def __init__(self):
+        self.has_gpu = torch.cuda.is_available()
+        # TODO: init config file here
+
+    def set_num_workers(self):
         config = configparser.ConfigParser()
         config.read_file(open(r'config.txt'))
-        if has_gpu:
+        if self.has_gpu:
             return int(config.get('GPU', 'NUM_WORKERS'))
         else:
             return int(config.get('CPU', 'NUM_WORKERS'))
 
+    def get_paths_dict(self):
+        """Get a dict of the paths for the case of cpu/gpu"""
+        config = configparser.ConfigParser()
+        config.read_file(open(r'config.txt'))
+        path_dict = {}
+        if self.has_gpu:
+            path_dict['train'] = config.get('GPU', 'TRAIN_PATH')
+            path_dict['val'] = config.get('GPU', 'VAL_PATH')
+            path_dict['test'] = config.get('GPU', 'TEST_PATH')
+            path_dict['pickle_train'] = config.get('GPU', 'PICKLE_TRAIN_PATH')
+            path_dict['pickle_val'] = config.get('GPU', 'PICKLE_VAL_PATH')
+            path_dict['pickle_test'] = config.get('GPU', 'PICKLE_TEST_PATH')
+            path_dict['labels'] = config.get('GPU', 'LABELS_PATH')
+        else:
+            path_dict['train'] = config.get('CPU', 'SMALL_TRAIN_PATH')
+            path_dict['val'] = config.get('CPU', 'SMALL_VAL_PATH')
+            # this will be empty at the moment
+            path_dict['test'] = config.get('CPU', 'TEST_PATH')
+            path_dict['pickle_train'] = config.get('CPU', 'PICKLE_S_TRAIN_PATH_V2')
+            path_dict['pickle_val'] = config.get('CPU', 'PICKLE_S_VAL_PATH_V2')
+            # this will be empty at the moment
+            path_dict['pickle_test'] = config.get('CPU', 'PICKLE_TEST_PATH')
+            path_dict['labels'] = config.get('CPU', 'LABELS_PATH')
 
-
-
+        return path_dict
 
 
