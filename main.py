1df629accd013c61d651acdb82a3cde28fddea5b
gidizil <gidizil@gmail.com>
2019-05-20 23:38:10 +0300

    supports fit_and_eval method

--- main.py
+++ main.py
@@ -13,36 +13,40 @@ config = configparser.ConfigParser()
 config.read_file(open(r'config.txt'))
 class_config = GPUConfig()
 path_dict = class_config.get_paths_dict()
-# PICKLE_S_TRAIN_PATH_V2 = config.get('PATHS', 'PICKLE_S_TRAIN_PATH_V2')
-# PICKLE_S_VAL_PATH_V2 = config.get('PATHS','PICKLE_S_VAL_PATH_V2')
 
 # 2A. get pickled data objects (unpickled) for training
-images_list_path = os.path.join(path_dict['train'], 'images_names_list.pickle')
-images_list = PickleImageData.unpickle_file(images_list_path)
-labels_dict_path = os.path.join(path_dict['train'], 'labels_dict.pickle')
-labels_dict = PickleImageData.unpickle_file(labels_dict_path)
+images_list_path = os.path.join(path_dict['pickle_train'], 'images_names_list.pickle')
+tr_images_list = PickleImageData.unpickle_file(images_list_path)
+labels_dict_path = os.path.join(path_dict['pickle_train'], 'labels_dict.pickle')
+tr_labels_dict = PickleImageData.unpickle_file(labels_dict_path)
 
-# 3. setting seeds:
+# 2B. get pickled data objects (unpickled) for validation
+images_list_path = os.path.join(path_dict['pickle_val'], 'images_names_list.pickle')
+val_images_list = PickleImageData.unpickle_file(images_list_path)
+labels_dict_path = os.path.join(path_dict['pickle_val'], 'labels_dict.pickle')
+val_labels_dict = PickleImageData.unpickle_file(labels_dict_path)
+
+# 3. setting seeds and parameters:
 SetSeeds.seed_torch()
+NUM_WORKERS = class_config.set_num_workers()
 
-# 4. Build Dataset and DataLoader for training
-train_set = CancerDataset(path_dict['train'], labels_dict, images_list)
-NUM_WORKERS = GPUConfig.set_num_workers()
+# 4A. Build Dataset and DataLoader for training
+train_set = CancerDataset(path_dict['pickle_train'], tr_labels_dict, tr_images_list)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=64,
                                            shuffle=True, num_workers=NUM_WORKERS,
                                            worker_init_fn=SetSeeds._init_fn)
 
+# 4B. Build Dataset and DataLoader for validation
+val_set = CancerDataset(path_dict['pickle_val'], val_labels_dict, val_images_list)
+
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=64,
+                                         shuffle=True, num_workers=NUM_WORKERS,
+                                         worker_init_fn=SetSeeds._init_fn)
+
 # 5. Train Network
 net = Net()
 net_classifier = Classifier(net)
 
-net_classifier.fit(train_loader)
-
-# 6. Predict on new samples
-images_list_path = os.path.join(path_dict['val'], 'images_names_list.pickle')
-images_list = PickleImageData.unpickle_file(images_list_path)
-labels_dict_path = os.path.join(path_dict['val'], 'labels_dict.pickle')
-labels_dict = PickleImageData.unpickle_file(labels_dict_path)
-
+net_classifier.fit_and_eval(train_loader, val_loader)
 # TODO: finish the rest to be able to make predicitions on new data.
