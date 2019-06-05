36b86fe2c9bfcf013435c7d77b57869aee7c3477
gidizil <gidizil@gmail.com>
2019-06-05 14:19:01 +0300

    class to hold different augementations

---
+++ transformations.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+
+""" ================================= """
+""" Class to hold everything that is  """
+""" related to pre-processing of the  """
+""" data and augmentations as well.   """
+""" Methods for training/testing with """
+""" augmentation will also be here    """
+""" ================================= """
+
+class Transforamtions:
+    pass
