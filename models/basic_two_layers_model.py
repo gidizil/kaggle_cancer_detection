21709fb50617663634f13c69d0fdea1fa0b5d067
gidizil <gidizil@gmail.com>
2019-06-16 13:00:02 +0300

    preparation for hadnling cropped images

--- models/basic_two_layers_model.py
+++ models/basic_two_layers_model.py
@@ -1,13 +1,18 @@
 import torch.nn as nn
 import torch.nn.functional as F
+from models.model_utils import ModelUtils
+
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, img_dims=None):
+
+        self.img_dims = img_dims
+
         super(Net, self).__init__()
         #TODO: Add config options
         self.conv1 = nn.Conv2d(3, 16, 5)
         self.conv1_bn_2d = nn.BatchNorm2d(16)
-        self.pool = nn.MaxPool2d(2,2)
+        self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(16, 128, 5)
         self.conv2_bn_2d = nn.BatchNorm2d(128)
         self.fc1 = nn.Linear(128*21*21, 120)
@@ -32,6 +37,8 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+
 
 
 
