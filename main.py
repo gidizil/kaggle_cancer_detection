1b3e4827aeb6b4d2a2cfbc66366fd4806bef89c3
gidizil <gidizil@gmail.com>
2019-07-04 23:05:46 +0300

    supporting resnet50

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import torchvision
 from data_utils_v2 import CancerDataset
 from data_utils_v2 import PickleImageData
 import configparser
-from models.basic_two_layers_model import Net
+#from models.basic_two_layers_model_copy import Net
+from models.resnet50 import Net
 from models.train_classifier import Classifier
 import os
 from general_utils import SetSeeds, GPUConfig, HyperParamsConfig
@@ -33,12 +34,15 @@ val_labels_dict = PickleImageData.unpickle_file(labels_dict_path)
 SetSeeds.seed_torch()
 
 # 4A. Set transformers, then build Dataset and DataLoader for training
-transformers = Transformations(crop_size=h_params_dict['center_crop'])
+transformers = Transformations(crop_size=h_params_dict['center_crop'],
+                               resize=h_params_dict['resize']
+                               )
 center_crop = transformers.center_crop
+resize = transformers.resize
 
 train_set = CancerDataset(path_dict['pickle_train'],
                           tr_labels_dict, tr_images_list,
-                          img_transform=center_crop)
+                          img_transform=resize)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=h_params_dict['batch_size'],
                                            shuffle=True, num_workers=h_params_dict['num_workers'],
@@ -48,14 +52,15 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=h_params_dict['
 # 4B. Build Dataset and DataLoader for validation
 val_set = CancerDataset(path_dict['pickle_val'],
                         val_labels_dict, val_images_list,
-                        img_transform=center_crop)
+                        img_transform=resize)
 
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=h_params_dict['batch_size'],
                                          shuffle=True, num_workers=h_params_dict['num_workers'],
                                          worker_init_fn=SetSeeds._init_fn)
 
 # 5. Train Network
-net = Net(h_params_dict['center_crop'])
+# net = Net(h_params_dict['center_crop'])
+net = Net()
 net_classifier = Classifier(net)
 
 net_classifier.fit_and_eval(train_loader, val_loader)
