f7eba0c8071166ed872d2694277acbcf7857821f
gidizil <gidizil@gmail.com>
2019-07-04 23:06:52 +0300

    support for BCE loss

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -5,7 +5,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from general_utils import GPUConfig, HyperParamsConfig
-
 import os
 
 
@@ -40,17 +39,6 @@ class Classifier:
         self._set_device()
         self.pass_device_to_gpu()
 
-    def plot_train_val_loss(self, train_loss_arr, val_loss_arr):
-        """ Plot the training and validation error after each epoch"""
-        path_dict = self.class_config.get_paths_dict()
-        plot_path = os.path.join(self.path_dict['plots'], 'train_val_loss.png')
-        plt.plot(range(1, self.epochs + 1), train_loss_arr, label='Train Loss')
-        plt.plot(range(1, self.epochs + 1), val_loss_arr, label='Validation Loss')
-        plt.legend(loc='upper right')
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.savefig(plot_path)
-
     def _set_device(self):
         """set device to gpu if possible"""
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -68,9 +56,12 @@ class Classifier:
     def set_loss_function(self):
         """setting loss function and optimization method."""
         # TODO: allow things that aren't adam
-        criterion = nn.CrossEntropyLoss()
-        optimizier = optim.Adam(self.classifier.parameters(),
-                                lr=0.001)
+        if self.h_params_dict['loss_func'] == 'cross_entropy':
+            criterion = nn.CrossEntropyLoss()
+        elif self.h_params_dict['loss_func'] == 'bce':
+            criterion = nn.BCELoss()
+
+        optimizier = optim.Adam(self.classifier.parameters())
         return criterion, optimizier
 
     def fit(self, train_loader):
@@ -89,7 +80,12 @@ class Classifier:
 
                 # Forward pass
                 scores = self.classifier(images)
-                loss = criterion(scores, labels)
+
+                if self.h_params_dict['loss'] == 'bce':
+                    print(type(labels.view(-1, 1).float()))
+                    loss = criterion(scores, labels.view(-1, 1).float())
+                elif self.h_params_dict['loss'] == 'cross_entropy':
+                    loss = criterion(scores, labels)
 
                 # Backward pass
                 optimizier.zero_grad()
@@ -124,34 +120,48 @@ class Classifier:
         """ loss on the entire validation set   """
         train_loss_arr = []
         val_loss_arr = []
+        accuracy_arr = []
         criterion, optimizier = self.set_loss_function()
         for epoch in range(self.epochs):
             train_loss = 0.0
             val_loss = 0.0
+            total_corrcets = 0.0
+            total_samples = 0.0
             # train and calculate loss in epoch
+            self.classifier.train()
             for i, (images, labels) in enumerate(train_loader):
                 # set to train mode
                 # TODO: is possible to set classifier outside training loop
-                self.classifier.train()
+
                 # pass data to gpu:
                 images = images.to(self.device)
                 labels = labels.to(self.device)
 
                 # Forward pass:
+                optimizier.zero_grad()
+
                 scores = self.classifier(images)
-                loss = criterion(scores, labels)
+                if self.h_params_dict['loss_func'] == 'bce':
+                    labels = labels.view(-1, 1).float()
+                    loss = criterion(scores, labels)
+                elif self.h_params_dict['loss_func'] == 'cross_entropy':
+                    loss = criterion(scores, labels)
 
                 # Backward pass and weight update:
-                optimizier.zero_grad()
                 loss.backward()
                 optimizier.step()
 
-                train_loss += loss.item()
+                train_loss += loss.item() * labels.size()[0]
+
 
             # set classifier to eval mode:
             self.classifier.eval()
             with torch.no_grad():
-                for i, (images, labels) in enumerate(val_loader):
+
+            #     for name, param in self.classifier.named_parameters():
+            #         if param.requires_grad:
+            #             print(name, param.data)
+            #     for i, (images, labels) in enumerate(val_loader):
 
                     # pass device to gpu
                     images = images.to(self.device)
@@ -159,15 +169,40 @@ class Classifier:
 
                     # Forward pass
                     scores = self.classifier(images)
-                    loss = criterion(scores, labels)
-                    val_loss += loss.item()
+                    if self.h_params_dict['loss_func'] == 'bce':
+                        labels = labels.view(-1, 1).float()
+                        loss = criterion(scores, labels)
+                    elif self.h_params_dict['loss_func'] == 'cross_entropy':
+                        loss = criterion(scores, labels)
 
-            train_loss_arr.append(train_loss)
-            val_loss_arr.append(val_loss)
+                    val_loss += loss.item() * labels.size()[0]
+
+                    # Calc Values for accuracy
+                    corrects_cnt, samples_cnt = self.calc_accuracy(scores, labels)
+                    total_corrcets += corrects_cnt
+                    total_samples += samples_cnt
+
+            print('Epoch:', epoch+1, ', Train Loss:', train_loss / len(train_loader.dataset))
+            print('Epoch:', epoch+1, ', val Loss:', val_loss / len(val_loader.dataset))
+            print('Validation Accuracy:', total_corrcets / total_samples)
+            accuracy_arr.append(total_corrcets / total_samples)
+            train_loss_arr.append(train_loss / len(train_loader.dataset))
+            val_loss_arr.append(val_loss / len(val_loader.dataset))
 
 
         # TODO: plot train and val loss for each epoch - Make sure everything goes down
         self.plot_train_val_loss(train_loss_arr, val_loss_arr)
+        self.plot_val_accuracy(accuracy_arr)
+
+    def calc_accuracy(self, scores, labels):
+        """Calculates the accuracy in the currenct  """
+        # classified_indices = scores.argmax(dim=1)
+        classified_indices = (scores >= 0.5).float()
+        #total_corrects = (classified_indices == labels).sum()
+        total_corrects = classified_indices.eq(labels.view_as(classified_indices)).sum().item()
+        total_samples = labels.size()[0]
+
+        return total_corrects, total_samples
 
     # TODO: add precision recall support
     def calc_precision_recall(self, scores, labels):
@@ -177,6 +212,27 @@ class Classifier:
     def visualize_training(self):
         """Visualize the predictions for images for each label"""
         pass
+
+    def plot_train_val_loss(self, train_loss_arr, val_loss_arr):
+        """ Plot the training and validation error after each epoch"""
+        plot_path = os.path.join(self.path_dict['plots'], 'train_val_loss.png')
+        plt.plot(range(1, self.epochs + 1), train_loss_arr, label='Train Loss')
+        plt.plot(range(1, self.epochs + 1), val_loss_arr, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.savefig(plot_path)
+        plt.close()
+
+    def plot_val_accuracy(self, val_accuracy_arr):
+        """Plots the validation accuracy through training"""
+        plot_path = os.path.join(self.path_dict['plots'], 'val_accuracy.png')
+        plt.plot(range(1, self.epochs + 1), val_accuracy_arr, label='Validation Accuracy')
+        plt.legend(loc='upper left')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.savefig(plot_path)
+        plt.close()
 
 
 
