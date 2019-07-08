fbd8b20df591afbaed2c8649ccb9263597ab4495
gidizil <gidizil@gmail.com>
2019-07-08 23:42:20 +0300

    handling transforms. Needs better solution

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import torch
-import torchvision
 from data_utils_v2 import CancerDataset
 from data_utils_v2 import PickleImageData
 import configparser
-#from models.basic_two_layers_model_copy import Net
-from models.resnet50 import Net
+from models.basic_two_layers_model_copy import Net
+from models.three_layers_model import Net
+from models.four_layers_model import Net
+# from models.resnet50 import Net
 from models.train_classifier import Classifier
 import os
 from general_utils import SetSeeds, GPUConfig, HyperParamsConfig
@@ -39,10 +40,14 @@ transformers = Transformations(crop_size=h_params_dict['center_crop'],
                                )
 center_crop = transformers.center_crop
 resize = transformers.resize
+crop_resize = transformers.crop_resize
+
+# Choose desired transform
+final_transform = crop_resize
 
 train_set = CancerDataset(path_dict['pickle_train'],
                           tr_labels_dict, tr_images_list,
-                          img_transform=resize)
+                          img_transform=final_transform)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=h_params_dict['batch_size'],
                                            shuffle=True, num_workers=h_params_dict['num_workers'],
@@ -52,19 +57,27 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=h_params_dict['
 # 4B. Build Dataset and DataLoader for validation
 val_set = CancerDataset(path_dict['pickle_val'],
                         val_labels_dict, val_images_list,
-                        img_transform=resize)
+                        img_transform=final_transform)
 
 val_loader = torch.utils.data.DataLoader(val_set, batch_size=h_params_dict['batch_size'],
                                          shuffle=True, num_workers=h_params_dict['num_workers'],
                                          worker_init_fn=SetSeeds._init_fn)
 
 # 5. Train Network
-# net = Net(h_params_dict['center_crop'])
-net = Net()
+if final_transform == center_crop:
+    net = Net(h_params_dict['center_crop'])
+elif final_transform == crop_resize:
+    net = Net(h_params_dict['resize'])
+else:
+    net = Net()
+
 net_classifier = Classifier(net)
 
 net_classifier.fit_and_eval(train_loader, val_loader)
+# net_classifier.fit(train_loader)
 
-#6. Test on unseen data
-# TODO: finish the rest to be able to make predictions on new data.
+# 6. Save Model
+model_name = 'basic_model_center_crop_48'
+model_path = os.path.join(path_dict['saved_models'], model_name + '.pt')
 
+torch.save(net_classifier.classifier.state_dict(), model_path)
